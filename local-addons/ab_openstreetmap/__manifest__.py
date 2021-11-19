7378e46792ff1276cd32ce85f2377ae68bc009ac
José Miguel Cordero <jmcordero@nunoa.cl>
2021-11-19 11:01:19 -0300

    feat(openstreetmap): add modified odoo module for openstreetmap widget

---
+++ local-addons/ab_openstreetmap/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Openstreetmap Widget",
+    'summary': """
+       Openstreetmap Widget
+    """,
+    'description': """
+        Show Openstreetmap in Form View
+        Required for works add a lat long field to the model
+    """,
+    'author': "PT. ISMATA NUSANTARA ABADI",
+    'website': "http://www.ismata.co.id",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    "qweb": ['static/src/xml/openstreetmap_template.xml'],
+    'data': ['views/templates.xml'],
+}
---
+++ local-addons/ab_openstreetmap/static/src/js/openstreetmap_widget.js
@@ -0,0 +1,97 @@
+odoo.define("ab_openstreetmap.openstreetmap_widget", function (require) {
+  "use strict";
+  var fieldRegistry = require("web.field_registry");
+  var abstractField = require("web.AbstractField");
+
+  var openstreetmap = abstractField.extend({
+    template: "openstreetmap_template",
+    start: function () {
+      var self = this;
+      this._super();
+      self._initMap();
+    },
+    _initMap: function () {
+      var self = this
+      $(document).ready(function () {
+        setTimeout(() => {
+          var lat = -33.45413198048182;
+          var lng = -70.59354128229793;
+          self.map = L.map('mapid').setView([lat, lng], 13);
+          
+          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          }).addTo(self.map);
+
+          // this.map = mymap;
+
+          // var edit = self.mode == "edit" ? true : false;
+          // self.marker = L.marker([lat, lng]).addTo(mymap);
+          
+          // marker.on("dragend", function (e) {
+          //   var latlng = e.target._latlng;
+          //   self.trigger_up("field_changed", {
+          //     dataPointID: self.dataPointID,
+          //     changes: {
+          //       lat: latlng.lat,
+          //       lng: latlng.lng,
+          //     },
+          //     viewType: self.viewType,
+          //   });
+          // });
+          
+          // if (edit) {
+          //   var geocode = L.Control.geocoder({
+          //     defaultMarkGeocode: false,
+          //   }).addTo(mymap);
+            
+          //   geocode.on("markgeocode", function (e) {
+          //     var lat = e.geocode.center.lat;
+          //     var lng = e.geocode.center.lng;
+              
+          //     mymap.flyTo([lat, lng]);
+          //     marker.setLatLng(new L.LatLng(lat, lng));
+          //     self.trigger_up("field_changed", {
+          //       dataPointID: self.dataPointID,
+          //       changes: {
+          //         lat: lat,
+          //         lng: lng,
+          //       },
+          //       viewType: self.viewType,
+          //     });
+          //   });
+          // }
+
+          var interval = setInterval(() => {
+            if (self.map && self.map._size.x > 0){
+              clearInterval(interval);
+            } else if (!document.getElementById("mapid")) {
+              clearInterval(interval);
+            }
+            window.dispatchEvent(new Event("resize"));
+          }, 500);
+        }, 100);
+
+      });
+    },
+
+    _renderReadonly: function () {
+      if (this.hasOwnProperty("map")) {
+        var lat_lon = this.value.split(",").map(Number);
+        if ((lat_lon[0] !==0) & (lat_lon[1] !== 0)) {
+          this.map.setView(lat_lon, 16);
+          if (this.marker != undefined) {
+            this.map.removeLayer(this.marker);
+          }        
+          this.marker = L.marker(lat_lon).addTo(this.map);
+        }
+      }
+    },
+
+    isSet: function () {
+      return true;
+    },
+  });
+  
+  fieldRegistry.add("openstreetmap", openstreetmap);
+});
