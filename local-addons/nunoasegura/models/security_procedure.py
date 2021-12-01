6974165791a82babc17df06de48da06367d86ad7
José Miguel Cordero <jmcordero@nunoa.cl>
2021-12-01 10:07:46 -0300

    refactor(nunoasegura-models): new fields to security procedure, refactor of categories

--- local-addons/nunoasegura/models/security_procedure.py
+++ local-addons/nunoasegura/models/security_procedure.py
@@ -1,48 +1,74 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import logging
+
+logging.basicConfig(format='[%(asctime)s] - %(name)s - %(funcName)s - %(levelname)s : %(message)s', level=logging.INFO)
+log = logging.getLogger(__name__)
 
 AVAILABLE_CHANNELS = [
     ('sosafe', 'SOSAFE'),
     ('telefono', 'Teléfono'),
     ('radial', 'Radial'),
-    ('camaras', 'Cámaras')
+    ('camaras', 'Cámaras'),
+    ('plan-barrial', 'Plan Barrial')
 ]
 
 class SecurityProcedureTypeCategory(models.Model):
     _name = 'nunoasegura.security.procedure.type.category'
 
     name = fields.Char(string="Nombre")
+    types = fields.One2many('nunoasegura.security.procedure.type', 'category', string="Tipos")
+    # types = fields.One2many('nunoasegura.security.procedure.type', string="Tipos")
+
 
 class SecurityProcedureType(models.Model):
     _name = 'nunoasegura.security.procedure.type'
 
     name = fields.Char(string="Nombre")
     category = fields.Many2one('nunoasegura.security.procedure.type.category', string="Categoría")
-    
+
+class Patrol(models.Model):
+    _name = 'nunoasegura.patrol'
+
+    name = fields.Char(string="Nombre")
     
 class SecurityProcedure(models.Model):
     _name = 'nunoasegura.security.procedure'
-    _inherit = ['tickets.ticket', 'tickets.geoplace']
+    _inherit = ['tickets.ticket', 'tickets.geoplace', 'mail.thread']
 
     channel = fields.Selection(AVAILABLE_CHANNELS, string="Canal")
     type = fields.Many2one('nunoasegura.security.procedure.type', string='Tipo')
+    operator = fields.Many2one('res.users', string="Operador", default=lambda self: self.env.user, readonly=True)
+    neighborhood_plan = fields.Many2one('nunoasegura.neighborhood.plan', string="Plan Barrial")
+    # inspectors = fields.Many2many('res.users', string='Inspectores')
+    inspectors = fields.Many2many('res.users', string='Inspectores', 
+        domain=lambda self: [("groups_id", "=", self.env.ref("nunoasegura.inspector_group").id)])
+    patrols = fields.Many2many('nunoasegura.patrol', string="Patrullas")
+    outcome = fields.Text("Resultado")
+
+    inspector_assigned_at = fields.Datetime("Asignación de inspector", readonly=True)
+    inspector_arrival_at = fields.Datetime("Llegada al lugar", readonly=True)
+    
+    @api.model
+    def create(self, data):
+        return super(SecurityProcedure, self.with_context(tracking_disable=True)).create(data)
+
+    @api.onchange('inspectors', 'patrols')
+    def set_inspector_assigned_at(self):
+        if (not self.inspector_assigned_at) and (self.inspectors or self.patrols):
+            for record in self:
+                record.inspector_assigned_at = fields.Datetime.now()
+            
+    
+    # def reset_geolocation(self):
+    #     return super().reset_geolocation()
 
 
+    def mark_inspector_arrival(self):
+        self.inspector_arrival_at = fields.Datetime.now()
 
-
-
-
-# class nunoasegura(models.Model):
-#     _name = 'nunoasegura.nunoasegura'
-#     _description = 'nunoasegura.nunoasegura'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+        # hora de creación
+        # hora de asignacion
+        # hora de llegada
+        # hora de cierre
